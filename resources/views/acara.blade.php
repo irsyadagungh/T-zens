7b2e3eeb6364960e2dcda6f582e422e5260469f6
irsyadagungh <irsyadagung08@gmail.com>
2023-05-11 20:40:16 +0700

    foto

--- resources/views/acara.blade.php
+++ resources/views/acara.blade.php
@@ -98,7 +98,6 @@
                 </div>
                 <div class="content-2">
                     <p class="type">{{ $acara['tipe_acara'] }}</p>
-
                     <p>{{ $acara['subscription'] }}</p>
                 </div>
             </div>
